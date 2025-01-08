0ca14885ae87fcbfb9432ff1f78e5428e466c5f3
dev2024 <oleg.sitnikov@icloud.com>
2025-01-08 22:50:57 +0300

    Initial Commit

---
+++ WorldNewsSwiftUIApp/WorldNewsSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldNewsSwiftUIAppApp.swift
+//  WorldNewsSwiftUIApp
+//
+//  Created by Oleg Sitnikov on 08.01.25.
+//
+
+import SwiftUI
+
+@main
+struct WorldNewsSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
