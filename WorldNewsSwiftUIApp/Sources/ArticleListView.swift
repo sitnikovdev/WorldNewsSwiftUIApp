efcf6c1845cd5ecf558986c68f7f350f18545fd2
dev2024 <oleg.sitnikov@icloud.com>
2025-01-22 12:43:11 +0300

    Add Images

--- WorldNewsSwiftUIApp/Sources/ArticleListView.swift
+++ WorldNewsSwiftUIApp/Sources/ArticleListView.swift
@@ -21,9 +21,32 @@ struct ArticleListView: View {
                                Text(item.article.author ?? "No author")
                                Spacer()
                                Text(item.article.title ?? "No title")
+                                   .font(.headline)
                                Spacer()
-                               Text(item.article.toImageUrl ?? "No image")
+                               if let imageUrl = item.article.toImageUrl {
+                                   AsyncImage(url: URL(string: imageUrl)) { phase in
+                                       switch phase {
+                                       case .empty:
+                                           ProgressView()
+                                               .progressViewStyle(.circular)
+                                               .frame(width: 50, height: 50)
+                                       case .success(let image):
+                                           image
+                                               .resizable()
+                                               .scaledToFill()
+                                               .frame(width: 300, height: 200)
+                                               .cornerRadius(10)
+
+                                       case .failure(let error):
+                                           Image(systemName: "exclamationmark.triangle.fill")
+                                       @unknown default:
+                                           EmptyView()
+                                       }
+                                   }
+                               }
                                Spacer()
+                               Spacer()
+                               Text("\(item.article.publishedAt ?? .now)")
                                Spacer()
                                Spacer()
                                Divider()
