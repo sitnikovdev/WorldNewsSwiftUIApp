372f12c2175dde738e22a9c633f1ec043d8fde5d
dev2024 <oleg.sitnikov@icloud.com>
2025-01-24 20:43:10 +0300

    Add sheet on article tapped

---
+++ WorldNewsSwiftUIApp/Sources/ArticleDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ArticleDetailView.swift
+//  WorldNewsSwiftUIApp
+//
+//  Created by Oleg Sitnikov on 24.01.25.
+//
+
+import SwiftUI
+
+struct ArticleDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ArticleDetailView()
+}
