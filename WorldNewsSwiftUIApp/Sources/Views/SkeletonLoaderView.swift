349908ffafe60da301620d1cae3e42eefbaa250a
dev2024 <oleg.sitnikov@icloud.com>
2025-01-27 14:19:09 +0300

    clean

--- WorldNewsSwiftUIApp/Sources/Views/SkeletonLoaderView.swift
+++ WorldNewsSwiftUIApp/Sources/Views/SkeletonLoaderView.swift
@@ -33,3 +33,37 @@ struct SkeletonLoaderView: View {
 #Preview {
     SkeletonLoaderView()
 }
+
+struct Shimmering: ViewModifier {
+    @State private var offset: CGFloat = -1
+
+    func body(content: Content) -> some View {
+        content
+            .overlay(
+                LinearGradient(
+                    gradient: Gradient(
+                        colors: [
+                            Color.gray.opacity(0.3),
+                            Color.gray.opacity(0.1),
+                            Color.gray.opacity(0.3),
+                        ]),
+                    startPoint: .topLeading,
+                    endPoint: .bottomTrailing
+                )
+                .mask(content)
+                .offset(x: offset)
+                .animation(
+                    .linear(duration: 1.5)
+                    .repeatForever(autoreverses: false), value: offset
+                ))
+                .onAppear {
+                    offset = UIScreen.main.bounds.width
+                }
+    }
+}
+
+extension View {
+    public func shimmering() -> some View {
+        modifier(Shimmering())
+    }
+}
