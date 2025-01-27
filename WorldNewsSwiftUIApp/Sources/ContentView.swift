8131c22cc87bce5154cc511dd39d5efe8542b00e
dev2024 <oleg.sitnikov@icloud.com>
2025-01-27 19:41:46 +0300

    save

--- WorldNewsSwiftUIApp/Sources/ContentView.swift
+++ WorldNewsSwiftUIApp/Sources/ContentView.swift
@@ -19,7 +19,6 @@ struct ContentView: View {
                 ArticleListView()
                     .task(id: viewModel.category, loadArticles)
                     .navigationTitle(viewModel.category.rawValue)
-                    .navigationBarTitleDisplayMode(.inline)
             }
         }
 
